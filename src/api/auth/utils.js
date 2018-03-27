df3e5cbacc6fef67d852f5cb48ea533b1cbc3c54
Alexandr Dubrovin <legys5@yandex.ru>
2018-03-27 23:18:44 +0300

    done cookies and sessions

--- src/api/auth/utils.js
+++ src/api/auth/utils.js
@@ -4,11 +4,16 @@ const formatUserRes = originalUser => ({
 });
 
 const checkAuth = (req, res, next) => {
+  // console.log("REQQqqqQQQQQ");
+  // eslint-disable-next-line
+  // for (const key in req) {
+  //   console.log(req[key]);
+  // }
   if (req.isAuthenticated()) {
     console.log("req.user", req.user);
     return next();
   }
-  return res.status(404).send({
+  return res.status(401).send({
     errors: req.session.errors,
     testError: "auth is missing"
   });
--- src/index.js
+++ src/index.js
@@ -2,29 +2,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
-const cookieSession = require("cookie-session");
+// const cookieSession = require("cookie-session");
+const session = require("express-session");
+// const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-// eslint-disable-next-line
-const passportSetup = require("./services/passport");
 const mongooseSetup = require("./services/mongoose");
-
 // connect to mongoDB
+mongooseSetup.setUpConnection();
 
 const auth = require("./api/auth");
 
 const app = express();
 
+// app.set("trust proxy", 1);
+app.use(bodyParser.json());
+// app.use(cookieParser());
+
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [process.env.COOKIE_KEY],
+//     secret: process.env.COOKIE_KEY
+//   })
+// );
 app.use(
-  cookieSession({
+  session({
     name: "session",
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY]
+    secret: process.env.COOKIE_KEY,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000 * 30
+    }
   })
 );
+
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
     credentials: true
   })
 );
@@ -32,10 +50,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongooseSetup.setUpConnection();
+// eslint-disable-next-line
+const passportSetup = require("./services/passport");
 
-app.use(bodyParser());
 app.use("/auth", auth);
+// app.use((err, req, res, next) => {});
 
 const port = process.env.PORT || 3000;
 
