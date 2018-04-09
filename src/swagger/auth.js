77c0d53dd8d975a5e2fc18e0d065eeb7ca91a2b1
Alexandr Dubrovin <legys5@yandex.ru>
2018-04-10 00:40:38 +0300

    split swagger; add autoincrement; fix models

---
+++ src/swagger/auth.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "/auth/google": {
+    get: {
+      tags: ["Auth"],
+      summary:
+        "Google OAuth endpoint; Call this for getting authorization modal; LOGIN or REGISTER from frontend!"
+    }
+  },
+  "/auth/google/callback": {
+    get: {
+      tags: ["Auth"],
+      summary:
+        "Send access token to get profile; LOGIN or REGISTER from frontend!",
+      parameters: [
+        {
+          in: "query",
+          name: "code",
+          required: true,
+          description: "code from /auth/google in browser's url",
+          schema: {
+            type: "string"
+          }
+        }
+      ],
+      produces: ["application/json"],
+      responses: {
+        "200": {
+          description: "OK",
+          schema: {
+            $ref: "#/definitions/Profile"
+          }
+        }
+      }
+    }
+  },
+  "/auth/profile": {
+    get: {
+      tags: ["Auth"],
+      summary: "Profile of authorized user",
+      produces: ["application/json"],
+      responses: {
+        "200": {
+          description: "User object",
+          schema: {
+            $ref: "#/definitions/Profile"
+          }
+        },
+        "404": {
+          description: "Auth is misssing; Check cookies"
+        }
+      }
+    }
+  }
+};
---
+++ src/swagger/models/profileModel.js
@@ -0,0 +1,14 @@
+module.exports = {
+  properties: {
+    _id: {
+      type: "string",
+      uniqueItems: true
+    },
+    googleId: {
+      type: "string"
+    },
+    displayName: {
+      type: "string"
+    }
+  }
+};
