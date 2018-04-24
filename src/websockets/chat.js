e5b12a1f3c4fb878e6d5f1f1e47907de432d3051
Alexandr Dubrovin <legys5@yandex.ru>
2018-04-24 23:57:25 +0300

    add chat support by ws

--- src/websockets/chat.js
+++ src/websockets/chat.js
@@ -8,13 +8,16 @@ const RoomMessages = mongoose.model("RoomMessages");
 // Array<{ users: []}>
 const rooms = [];
 const subscribeOnChat = io => {
-  console.log("Subscribe invoed");
+  console.log("Subscribe invoked");
   const chat = io.of("/chat");
 
   chat.on("connection", socket => {
     socket.send(JSON.stringify({ msg: "user joined" }));
     socket.on("message", message => {
       console.log("MESSAGE", message);
+    });
+    socket.on("disconnect", () => {
+      socket.disconnect(true);
     });
     socket.on("joinRoom", async data => {
       try {
@@ -36,50 +39,71 @@ const subscribeOnChat = io => {
               "roomMessages for this room doesn't exists"
             );
           }
-          console.log("messagesForRoom", messagesForRoom);
-          const roomObj = {
-            users: [data.userId],
-            messages: messagesForRoom.messages
-          };
-          if (rooms[roomId]) {
-            if (!rooms[roomId].users.some(user => user === data.userId)) {
-              rooms[roomId].users.push(data.userId);
-            }
-          } else {
-            rooms[roomId] = roomObj;
-          }
-          socket.emit("joined", rooms[roomId]);
+          socket.room = roomId;
+          socket.username = allowedUser.displayName;
+          socket.join(roomId);
+          socket.emit(
+            "updateChat",
+            "SERVER",
+            `You have connected to chat${roomId}`
+          );
+          socket.broadcast
+            .to(roomId)
+            .emit(
+              "updatechat",
+              "SERVER",
+              `${socket.username} has connected to this room`
+            );
+          console.log("rooms", socket.rooms);
+
+          // const roomObj = {
+          //   users: [data.userId],
+          //   messages: messagesForRoom.messages
+          // };
+          // if (rooms[roomId]) {
+          //   if (!rooms[roomId].users.some(user => user === data.userId)) {
+          //     rooms[roomId].users.push(data.userId);
+          //   }
+          // } else {
+          //   rooms[roomId] = roomObj;
+          // }
+          // socket.emit("joined", rooms[roomId]);
         } else {
           socket.emit("serverError", "user doesn't have acess to this chat");
         }
       } catch (error) {
-        socket.emit("serverError", error);
-        throw error;
-      }
-    });
-    socket.on("newMessage", async data => {
-      try {
-        console.log("newMessage", data, rooms[data.roomId]);
-        const newMessage = {
-          value: data.value,
-          createdBy: {
-            userId: data.userId
-          }
-        };
-        rooms[data.roomId].messages.push(newMessage);
-        console.log(rooms);
-        await RoomMessages.update(
-          { roomId: data.roomId },
-          { $push: { messages: newMessage } }
-        );
-        socket.emit("newMessage", newMessage);
-      } catch (error) {
-        socket.emit("serverError", `message sending is failed${error}`);
+        console.log("ERROR; handle this", error);
+        socket.emit("serverError", "error");
         throw error;
       }
     });
 
+    socket.on("newMessage", async data => {
+      try {
+        console.log("new message", data);
+        chat.to(socket.room).emit("updateChat", data);
+        // console.log("newMessage", data, rooms[data.roomId]);
+        // const newMessage = {
+        //   value: data.value,
+        //   createdBy: {
+        //     userId: data.userId
+        //   }
+        // };
+        // rooms[data.roomId].messages.push(newMessage);
+        // console.log(rooms);
+        // await RoomMessages.update(
+        //   { roomId: data.roomId },
+        //   { $push: { messages: newMessage } }
+        // );
+        // socket.emit("newMessage", newMessage);
+      } catch (error) {
+        // socket.emit("serverError", `message sending is failed${error}`);
+        // throw error;
+      }
+    });
+
     socket.on("close", () => {
+      socket.leave(socket.room);
       socket.disconnect(true);
       console.log("closed");
     });
