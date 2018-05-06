f17c6e1e74d38723c38d1acc8de97d704d50c884
Alexandr Dubrovin <legys5@yandex.ru>
2018-05-06 12:32:46 +0300

    add transports to socket io

--- src/websockets/index.js
+++ src/websockets/index.js
@@ -3,6 +3,15 @@ const { subscribeOnChat } = require("./chat");
 module.exports = {
   setUpConnection(io) {
     subscribeOnChat(io);
+    io.set("transports", [
+      "websocket",
+      "flashsocket",
+      "htmlfile",
+      "xhr-polling",
+      "jsonp-polling",
+      "polling"
+    ]);
+    io.set("polling duration", 10);
     setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
   }
 };
